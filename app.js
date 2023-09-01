6b4257b467077862cd77c12230acaa287c2c99c7
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2023-08-31 22:45:49 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,6 @@
+//Criei esse projeto para documentar o meu processo de aprendizagem em back-end
+
+//para fazer jus ao projeto, irein transformar isso aqui em uma api quee será consumida por um front
+
+//irei utilizar o banco de dados postgresql, qu estou aprendendo a utilizar e o sequelize como um orm.
+
